26beff763bbc3ae7466e9faae5c552ac559cd056
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-07 17:15:40 -0500

    Update RockPaperScissors.hs

--- Homework/Lecture7/RockPaperScissors.hs
+++ Homework/Lecture7/RockPaperScissors.hs
@@ -25,20 +25,20 @@ module Week07.RockPaperScissors
 
 import           Control.Monad                hiding (fmap)
 import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Monoid                  (Last (..))
 import           Data.Text                    (Text, pack)
 import           GHC.Generics                 (Generic)
-import           Plutus.Contract              as Contract hiding (when)
-import           Plutus.Contract.StateMachine
-import qualified PlutusTx
-import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
 import           Ledger                       hiding (singleton)
 import           Ledger.Ada                   as Ada
 import           Ledger.Constraints           as Constraints
 import           Ledger.Typed.Tx
 import qualified Ledger.Typed.Scripts         as Scripts
-import           Ledger.Value
+import           Plutus.Contract              as Contract
+import           Plutus.Contract.StateMachine
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
 import           Playground.Contract          (ToSchema)
-import           Prelude                      (Semigroup (..))
+import           Prelude                      (Semigroup (..), Show (..), String)
 import qualified Prelude
 
 data Game = Game
